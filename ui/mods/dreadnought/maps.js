b154c2d7226c8f1c9aa7c173dafa1930aff6ed5d
dreadnought101 <michaeljonesrox@gmail.com>
2024-07-23 22:15:08 -0600

    Crest update

--- ui/mods/dreadnought/maps.js
+++ ui/mods/dreadnought/maps.js
@@ -7,6 +7,7 @@ cShareSystems.load_pas("Dreadnought Map Pack-dev", [
     "coui://ui/mods/dreadnought/systems/1v1_Molg_1.3.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Temple-of-the-Monkey-God_2.4.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Kazranaut_2.7.pas",
+    "coui://ui/mods/dreadnought/systems/1v1_Crest_1.2.pas",
     "coui://ui/mods/dreadnought/systems/11v11_Aquamax_1.0.pas",
     "coui://ui/mods/dreadnought/systems/16v16_Aquamax_1.0.pas",
     "coui://ui/mods/dreadnought/systems/16v16_SUPER_Aquamax_1.2.6.pas",
