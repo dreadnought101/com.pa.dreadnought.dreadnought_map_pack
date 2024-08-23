927e753f307c8033423a207700fe0c72f48cdadd
dreadnought101 <michaeljonesrox@gmail.com>
2024-08-22 19:08:08 -0600

    1.8

--- ui/mods/dreadnought/maps.js
+++ ui/mods/dreadnought/maps.js
@@ -7,6 +7,7 @@ cShareSystems.load_pas("Dreadnought Map Pack", [
     "coui://ui/mods/dreadnought/systems/1v1_yukon-ho_3.2.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Molg_1.3.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Nastolda_1.4.pas",
+    "coui://ui/mods/dreadnought/systems/1v1_Oasis_1.0.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Rustad_1.24.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Smoker_1.4.pas",
     "coui://ui/mods/dreadnought/systems/1v1_Temple-of-the-Monkey-God_2.4.pas",
