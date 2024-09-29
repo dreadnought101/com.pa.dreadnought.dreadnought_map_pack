43e3200e2a01fbb48c4af4e6050614a1dd6d2c82
dreadnought101 <michaeljonesrox@gmail.com>
2024-09-28 18:00:12 -0600

    Snapper update

--- ui/mods/dreadnought/maps.js
+++ ui/mods/dreadnought/maps.js
@@ -20,7 +20,7 @@ cShareSystems.load_pas("Dreadnought Map Pack", [
         "coui://ui/mods/dreadnought/systems/1v1_Station_53C_2.0.pas",
         "coui://ui/mods/dreadnought/systems/1v1_Temple-of-the-Monkey-God_2.4.pas",
         "coui://ui/mods/dreadnought/systems/1v1_Kazranaut_2.7.pas",
-        "coui://ui/mods/dreadnought/systems/1v1_Snapper_1.2.pas",
+        "coui://ui/mods/dreadnought/systems/1v1_Snapper_1.3.pas",
         "coui://ui/mods/dreadnought/systems/1v1_Rustad_1.24.pas",
         "coui://ui/mods/dreadnought/systems/1v1_yukon-ho_3.2.pas",
         "coui://ui/mods/dreadnought/systems/11v11_Aquamax_1.0.pas",
